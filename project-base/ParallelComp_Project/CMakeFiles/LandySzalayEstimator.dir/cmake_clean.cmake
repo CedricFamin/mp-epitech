7da4f854163de13cb6b26be17ebf4299a78fd3a0
famin_c <cedric.famin@gmail.com>
2013-01-20 21:59:07 +0100

    Initialisation des proj Suppression des singleton
    Signed-off-by: famin_c <cedric.famin@gmail.com>

---
+++ project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/LandySzalayEstimator.dir/sources/htm.cpp.o"
+  "CMakeFiles/LandySzalayEstimator.dir/sources/htmasciiparser.cpp.o"
+  "CMakeFiles/LandySzalayEstimator.dir/sources/logservice.cpp.o"
+  "CMakeFiles/LandySzalayEstimator.dir/sources/main.cpp.o"
+  "CMakeFiles/LandySzalayEstimator.dir/sources/trixel.cpp.o"
+  "bin/LandySzalayEstimator.pdb"
+  "bin/LandySzalayEstimator"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/LandySzalayEstimator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ project/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/sources/htm.cpp" "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/sources/htm.cpp.o"
+  "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/sources/htmasciiparser.cpp" "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/sources/htmasciiparser.cpp.o"
+  "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/sources/logservice.cpp" "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/sources/logservice.cpp.o"
+  "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/sources/main.cpp" "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/sources/main.cpp.o"
+  "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/sources/trixel.cpp" "/Users/Pamilator/Work/mp-epitech/project-base/ParallelComp_Project/CMakeFiles/LandySzalayEstimator.dir/sources/trixel.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "/opt/local/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
